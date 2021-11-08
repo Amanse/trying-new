7843d7ca44bf11ae4cea66a8430ffe6a5e33ef61
Aman <amansetiarjp@gmail.com>
2021-11-08 18:42:41 +0530

    Strip1

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const diskinfo = require("./utils/diskinfo");
 const status = require("./utils/status");
 const { getFiles, sendFileStream, getAuthURL, getAuthToken } = require("./utils/gdrive");
 
-const search = require("./routes/search");
 const details = require("./routes/details");
 const torrent = require("./routes/torrent");
 
@@ -35,40 +34,7 @@ server.get("/logs", (req, res) => res.sendFile("logs.txt", { root: __dirname }))
 
 server.use("/downloads", express.static("downloads"), serveIndex("downloads", { icons: true }));
 
-server.use("/api/v1/drive/folder", async (req, res) => {
-  const folderId = req.query.id;
-  res.send(await getFiles(folderId));
-});
-
-server.use("/api/v1/drive/file/:id", sendFileStream);
-
-server.use("/api/v1/drive/getAuthURL", (req, res) => {
-  const CLIENT_ID = req.query.clientId;
-  const CLIENT_SECRET = req.query.clientSecret;
-
-  if (!CLIENT_ID || !CLIENT_SECRET) {
-    res.send(JSON.stringify({ error: "Client Id and secret are required" }));
-  } else {
-    const authURL = getAuthURL(CLIENT_ID, CLIENT_SECRET);
-    res.send(JSON.stringify({ error: "", authURL }));
-  }
-});
-
-server.use("/api/v1/drive/getAuthToken", async (req, res) => {
-  const CLIENT_ID = req.query.clientId;
-  const CLIENT_SECRET = req.query.clientSecret;
-  const AUTH_CODE = req.query.authCode;
-
-  if (!CLIENT_ID || !CLIENT_SECRET || !AUTH_CODE) {
-    res.send(JSON.stringify({ error: "Client Id and secret and auth code are required" }));
-  } else {
-    const token = await getAuthToken(CLIENT_ID, CLIENT_SECRET, AUTH_CODE);
-    res.send(JSON.stringify({ token, error: "" }));
-  }
-});
-
 server.use("/api/v1/torrent", torrent);
-server.use("/api/v1/search", search);
 server.use("/api/v1/details", details);
 
 server.get("/api/v1/uptime", async (req, res) => {
