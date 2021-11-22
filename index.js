225fa23f7d0d53d29c172305bf35d866b3cc252a
aman <amansetiarjp@gmail.com>
2021-11-22 08:46:20 +0000

    Gcp strip

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const bodyParser = require("body-parser");
 const serveIndex = require("serve-index");
 
 const humanTime = require("./utils/humanTime");
-const keepalive = require("./utils/keepalive");
+//const keepalive = require("./utils/keepalive");
 const diskinfo = require("./utils/diskinfo");
 const status = require("./utils/status");
-const { getFiles, sendFileStream, getAuthURL, getAuthToken } = require("./utils/gdrive");
+//const { getFiles, sendFileStream, getAuthURL, getAuthToken } = require("./utils/gdrive");
 
-const search = require("./routes/search");
-const details = require("./routes/details");
+//const search = require("./routes/search");
+//const details = require("./routes/details");
 const torrent = require("./routes/torrent");
 
 const dev = process.env.NODE_ENV !== "production";
@@ -19,7 +19,6 @@ const PORT = parseInt(process.env.PORT, 10) || 3000;
 
 const server = express();
 
-keepalive();
 
 server.use(compression());
 server.use(bodyParser.json());
@@ -35,41 +34,9 @@ server.get("/logs", (req, res) => res.sendFile("logs.txt", { root: __dirname }))
 
 server.use("/downloads", express.static("downloads"), serveIndex("downloads", { icons: true }));
 
-server.use("/api/v1/drive/folder", async (req, res) => {
-  const folderId = req.query.id;
-  res.send(await getFiles(folderId));
-});
-
-server.use("/api/v1/drive/file/:id", sendFileStream);
-
-server.use("/api/v1/drive/getAuthURL", (req, res) => {
-  const CLIENT_ID = req.query.clientId;
-  const CLIENT_SECRET = req.query.clientSecret;
-
-  if (!CLIENT_ID || !CLIENT_SECRET) {
-    res.send(JSON.stringify({ error: "Client Id and secret are required" }));
-  } else {
-    const authURL = getAuthURL(CLIENT_ID, CLIENT_SECRET);
-    res.send(JSON.stringify({ error: "", authURL }));
-  }
-});
-
-server.use("/api/v1/drive/getAuthToken", async (req, res) => {
-  const CLIENT_ID = req.query.clientId;
-  const CLIENT_SECRET = req.query.clientSecret;
-  const AUTH_CODE = req.query.authCode;
-
-  if (!CLIENT_ID || !CLIENT_SECRET || !AUTH_CODE) {
-    res.send(JSON.stringify({ error: "Client Id and secret and auth code are required" }));
-  } else {
-    const token = await getAuthToken(CLIENT_ID, CLIENT_SECRET, AUTH_CODE);
-    res.send(JSON.stringify({ token, error: "" }));
-  }
-});
-
 server.use("/api/v1/torrent", torrent);
-server.use("/api/v1/search", search);
-server.use("/api/v1/details", details);
+//server.use("/api/v1/search", search);
+//server.use("/api/v1/details", details);
 
 server.get("/api/v1/uptime", async (req, res) => {
   res.send({ uptime: humanTime(process.uptime() * 1000) });
--- routes/torrent.js
+++ routes/torrent.js
@@ -1,37 +1,14 @@
 const express = require("express");
 const telegram = require("node-telegram-bot-api");
 const Torrent = require("../lib/torrent");
-const botInit = require("../lib/bot");
+//const botInit = require("../lib/bot");
 const torrent = new Torrent();
 
 const dev = process.env.NODE_ENV !== "production";
-const site = dev ? require("../config").site : process.env.SITE;
-const token = dev ? require("../config").telegramToken : process.env.TELEGRAM_TOKEN;
+//const site = dev ? require("../config").site : process.env.SITE;
+//const token = dev ? require("../config").telegramToken : process.env.TELEGRAM_TOKEN;
 
 const router = express.Router();
-
-if (!token)
-  console.log(
-    "Set telegram token env var to start telegram bot. Read docs at https://github.com/patheticGeek/torrent-aio-bot"
-  );
-
-if (site && token) {
-  const botOptions = dev ? { polling: true } : {};
-  const bot = new telegram(token, botOptions);
-  if (!dev) {
-    router.post("/bot", (req, res) => {
-      bot.processUpdate(req.body);
-      res.sendStatus(200);
-    });
-    bot.setWebHook(`${site}api/v1/torrent/bot`);
-  } else {
-    router.post("/bot", (req, res) => {
-      res.sendStatus(200);
-    });
-  }
-  botInit(torrent, bot);
-  console.log("Bot ready");
-}
 
 router.get("/download", (req, res) => {
   const link = req.query.link;
